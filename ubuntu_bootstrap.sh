ebaed11d61a7a6ca83747c8dd595ed7d4207b42a
RichardDevers <ritchiedev@hotmail.com>
2017-02-14 16:38:42 +0100

    second commit  :/

---
+++ ubuntu_bootstrap.sh
@@ -0,0 +1,40 @@
+add-apt-repository ppa:fkrull/deadsnakes -y # add deadsnakes repository for python version
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - # add nodejs repository
+
+# update /upgrade
+apt-get update &&  apt-get upgrade -y
+
+# install pip, virtualenv and nodejs an npm
+apt-get install -y python-pip python-virtualenv nodejs npm htop iotop
+pip install --upgrade --user boto3 aws-shell httpie awscli chalice requests
+apt-get install -y python3.5# this one is done AFTER the installation of virtualenv !
+
+# Virtualenv bootstrap
+echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
+echo  'mkdir -p $WORKON_HOME' >> ~/.bashrc
+echo  'source ~/.local/bin/virtualenvwrapper.sh' >> ~./bashrc
+source ~/.bashrc #recharge bashrc
+
+# Virtualenv creation
+virtualenv p27 -p /usr/bin/python2.7
+virtualenv p35 -p /usr/bin/python35
+
+# Install framework serverless
+npm install -g serverless
+
+# Create hashicorp folder
+mkdir ~/hashicorp ~/hashicorp/terraform ~/hashicorp/packer
+
+# Install terraform
+cd ~/hashicorp/terraform
+wget https://releases.hashicorp.com/terraform/0.8.6/terraform_0.8.6_linux_amd64.zip
+unzip terraform_0.8.6_linux_amd64.zip
+rm terraform_0.8.6_linux_amd64.zip
+export PATH=$PATH:~/hashicorp/terraform
+
+# Install packer
+cd ~/hashicorp/packer
+wget https://releases.hashicorp.com/packer/0.12.2/packer_0.12.2_linux_amd64.zip
+unzip packer_0.12.2_linux_amd64.zip
+rm packer_0.12.2_linux_amd64.zip
+export PATH=$PATH:~/hashicorp/packer
