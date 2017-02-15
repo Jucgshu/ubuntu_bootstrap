5c022c53d23c85b08fd6380bfa2e656a2dab77f9
RichardDevers <ritchiedev@hotmail.com>
2017-02-15 15:49:09 +0100

    Minor correction

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -55,4 +55,5 @@ sudo usermod -G vboxusers -a $USER
 # Chrome
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
 apt-get install -y google-chrome-stable
