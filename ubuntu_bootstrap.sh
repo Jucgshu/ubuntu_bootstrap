82db043204e48510a245b4dd3f813d55398fc354
Richard Devers <ritchiedev@hotmail.com>
2017-02-16 04:11:15 -0500

    Add git init

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -57,3 +57,7 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 apt-get update
 apt-get install -y google-chrome-stable
+
+# Git config
+git config --global user.name "Richard Devers"
+git config --global user.email "ritchiedev@hotmail.com"
