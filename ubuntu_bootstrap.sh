5ebcaf7aa38fd48bbb4e43bc1accf13b045c37b6
Richard Devers <richarddevers@users.noreply.github.com>
2017-03-08 20:43:17 +0100

    Update ubuntu_bootstrap.sh

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -5,10 +5,7 @@ apt-get install curl
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 # install pip, virtualenv and nodejs an npm
-apt-get install -fy python-pip python-virtualenv nodejs htop iotop git jq keepass2 remmina putty mysql-workbench variety
-
-# Install Virtualenvwrapper
-pip install --user virtualenvwrapper
+apt-get install -fy python-pip python-virtualenv nodejs htop iotop git jq keepass2 remmina putty mysql-workbench variety virtualenvwrapper
 
 # Virtualenvwrapper bootstrap
 echo 'export WORKON_HOME=~/.virtualenvs' >> ~/.bashrc
